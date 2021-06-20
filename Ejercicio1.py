2b66e815dce125fd554a0e11e7cf9a2e736bc55c
Claudia-Arely-Perez-Mayo <84484574+Claudia-Arely-Perez-Mayo@users.noreply.github.com>
2021-06-20 00:24:52 -0500

    Create Ejercicio1.py
    Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def mostar_inicio():
+    contenido_html = """
+    <html>
+    <head>
+        <title>Equipo1</title>
+    </head>
+    <body>
+        <h3>Bienvenidos</h3>
+        <p>Este sitio pertenece al Equipo1 y mostrará los datos de los integrantes</p>
+		<a href="claudia.html"> Claudia Arely </a>
+    </body>
+	</html>
+    """
+    return HTMLResponse(content=contenido_html, status_code=200)
+	
